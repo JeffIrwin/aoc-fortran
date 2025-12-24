b0b8fa63fb76ab8ef7def2281d1074f1060c9cb5
Jeff Irwin <jirwin505@gmail.com>
2025-12-23 22:20:07 -0500

    refactor cmake include

--- template/flags.cmake
+++ template/flags.cmake
@@ -1,8 +1,4 @@
 
-set(SRCS
-	main.F90
-)
-
 if(NOT CMAKE_BUILD_TYPE)
 	set(CMAKE_BUILD_TYPE "Debug" CACHE STRING "Choose Release or Debug" FORCE)
 endif()
@@ -20,16 +16,6 @@ add_compile_options(-Wno-maybe-uninitialized)  # ignore incorrect warnings
 add_compile_options(-Wno-uninitialized)
 add_compile_options(-fimplicit-none -Werror=implicit-interface)
 
-set(LIB_DIR "${CMAKE_SOURCE_DIR}/../../src")
-set(LIB_BUILD_DIR "${LIB_DIR}/build/${CMAKE_BUILD_TYPE}")
-add_subdirectory(${LIB_DIR} ${LIB_BUILD_DIR})
-
 # Set module include dir
 add_compile_options("-I${LIB_BUILD_DIR}")
-
-# Put exe in current dir
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
-
-add_executable(${PROJECT} ${SRCS})
-target_link_libraries(${PROJECT} aoc-fortran)
 
